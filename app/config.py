f544d6a20fae04a628efd051755153ec3f48a1fe
kotegaeshi3000 <ostap-shevchenko@inbox.ru>
2026-07-28 09:19:56 +0300

    space out water reminder windows: 10-12/13-15/16-18/19-22

--- app/config.py
+++ app/config.py
@@ -9,9 +9,9 @@ PROACTIVE_WINDOW_END = time(19, 0)
 
 WATER_REMINDER_WINDOWS = [
     (time(10, 0), time(12, 0)),
-    (time(12, 0), time(15, 0)),
-    (time(15, 0), time(18, 0)),
-    (time(18, 0), time(22, 0)),
+    (time(13, 0), time(15, 0)),
+    (time(16, 0), time(18, 0)),
+    (time(19, 0), time(22, 0)),
 ]
 WATER_REMINDER_TEXTS = [
     "Эй, а водичку сегодня пил?",
